ed61c7b038266712300000253e2dcf980db41d6b
Vink64 <graci.victorrocha@gmail.com>
2024-04-24 17:11:12 -0300

    design da tela de login

--- app/src/main/java/com/example/eviene/Login.kt
+++ app/src/main/java/com/example/eviene/Login.kt
@@ -3,9 +3,9 @@ package com.example.eviene
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login)
     }
 }
