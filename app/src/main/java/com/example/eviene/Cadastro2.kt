13f3abfca35772b8fea49446ce0ef186b921e77b
Vink64 <graci.victorrocha@gmail.com>
2024-04-29 14:17:43 -0300

    Telas de cadastro

---
+++ app/src/main/java/com/example/eviene/Cadastro2.kt
@@ -0,0 +1,11 @@
+package com.example.eviene
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Cadastro2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro2)
+    }
+}
