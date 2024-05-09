f71ef6b148630081619e8e9c49d9818b30a06274
Vink64 <graci.victorrocha@gmail.com>
2024-05-09 00:56:16 -0300

    MainActivity

--- app/src/main/java/com/example/eviene/Login.kt
+++ app/src/main/java/com/example/eviene/Login.kt
@@ -1,11 +1,24 @@
 package com.example.eviene
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Login : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
+
+        val btnLogar=findViewById<Button>(R.id.btnLogEntrar)
+        btnLogar.setOnClickListener{
+            val nextPage = Intent(this,MainActivity::class.java);
+            startActivity(nextPage);
+            finish();
+        }
+
     }
 }
--- app/src/main/java/com/example/eviene/MainActivity.kt
+++ app/src/main/java/com/example/eviene/MainActivity.kt
@@ -2,13 +2,38 @@ package com.example.eviene
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.eviene.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.bottomNav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.pesquisar -> replaceFragment(PesquisarFragment())
+                R.id.postar -> replaceFragment(PostarFragment())
+                R.id.perfil -> replaceFragment(PerfilFragment())
+                else->{}
+            }
+            true
+        }
+
     }
+
+    private fun replaceFragment(fragment : Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.container,fragment)
+        fragmentTransaction.commit()
+    }
+
+
 }
