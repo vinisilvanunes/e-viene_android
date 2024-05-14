49a1bd9526eb4ffb86e2ca82aa128498d67407a4
Vink64 <graci.victorrocha@gmail.com>
2024-05-14 17:53:10 -0300

    experimentos, nada demais

--- app/src/main/java/com/example/eviene/MainActivity.kt
+++ app/src/main/java/com/example/eviene/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        replaceFragment(HomeFragment())
 
 
         binding.bottomNav.setOnItemSelectedListener {
