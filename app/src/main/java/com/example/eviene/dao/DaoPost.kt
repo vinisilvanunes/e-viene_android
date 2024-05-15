e0bcf2f207f25b1a32babcf543cd77eb115266ff
Vink64 <graci.victorrocha@gmail.com>
2024-05-15 18:00:06 -0300

    item_post,Dao,Adapter,Model

---
+++ app/src/main/java/com/example/eviene/dao/DaoPost.kt
@@ -0,0 +1,13 @@
+package com.example.eviene.dao
+
+import java.util.ArrayList
+import com.example.eviene.models.Post
+
+class DaoPost {
+    fun listaContatos(): ArrayList<Post> {
+        val list:ArrayList<Post> = ArrayList()
+        list.add(Post("1", "", "Myke", "Só sei que nada sei",""))
+
+        return list
+    }
+}
