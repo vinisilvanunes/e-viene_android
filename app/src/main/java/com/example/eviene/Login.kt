2b81106d4a97b2e08a3dbad079d62809af4d9bd6
Vink64 <graci.victorrocha@gmail.com>
2024-05-15 18:47:44 -0300

    mudanças para vizualização apenas

--- app/src/main/java/com/example/eviene/Login.kt
+++ app/src/main/java/com/example/eviene/Login.kt
@@ -13,12 +13,19 @@ class Login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
 
+//        val txtLogCadastrarSe=findViewById<Button>(R.id.txtLogCadastrarSe)
         val btnLogar=findViewById<Button>(R.id.btnLogEntrar)
+
         btnLogar.setOnClickListener{
             val nextPage = Intent(this,MainActivity::class.java);
             startActivity(nextPage);
             finish();
         }
+//        txtLogCadastrarSe.setOnClickListener{
+//            val nextPage = Intent(this,Cadastro::class.java);
+//            startActivity(nextPage);
+//            finish();
+//        }
 
     }
 }
