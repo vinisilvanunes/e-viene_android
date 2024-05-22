47f3b70ffe45bd493046e5fa28d03bd9a0889738
Vink64 <graci.victorrocha@gmail.com>
2024-05-22 18:12:14 -0300

    fragmento visuais e botões return

--- app/src/main/java/com/example/eviene/Login.kt
+++ app/src/main/java/com/example/eviene/Login.kt
@@ -4,28 +4,31 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import com.example.eviene.databinding.LoginBinding
 
 class Login : AppCompatActivity() {
 
-
+    private lateinit var binding: LoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login)
+        binding = LoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-//        val txtLogCadastrarSe=findViewById<Button>(R.id.txtLogCadastrarSe)
+        val txtLogCadastrarSe=findViewById<TextView>(R.id.txtLogCadastrarSe)
         val btnLogar=findViewById<Button>(R.id.btnLogEntrar)
 
         btnLogar.setOnClickListener{
-            val nextPage = Intent(this,MainActivity::class.java);
-            startActivity(nextPage);
+            val i = Intent(this,MainActivity::class.java);
+            startActivity(i);
             finish();
         }
-//        txtLogCadastrarSe.setOnClickListener{
-//            val nextPage = Intent(this,Cadastro::class.java);
-//            startActivity(nextPage);
-//            finish();
-//        }
+        txtLogCadastrarSe.setOnClickListener{
+            val i = Intent(this,Cadastro::class.java);
+            startActivity(i);
+            finish();
+        }
 
     }
 }
